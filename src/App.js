d35c1040c1feef338b0287aa932535ab712bfadf
jaredgreen <jgreen2@pillartechnology.com>
2019-01-03 14:39:05 -0600

    Initial create react-app for FizzBuzz Kata. Now with UI components

--- src/App.js
+++ src/App.js
@@ -6,20 +6,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <br></br>
+        <label id='instructions'>Enter a number in the text area to play!</label>
+        <br></br>
+        <br></br>
+        <input id='numberInput' type="number"></input>
+        <br></br>
+        <button id='submitButton'>Submit</button>
+        <br></br>
+        <label id='result'></label>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { mount } from 'enzyme'
+
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('Page Contains Basic Elements', () => {
+  var app;
+
+  beforeEach(() => {
+    app = mount(<App />)
+  });
+
+  it('should have a button Submit', () => {
+    expect(app.find('#submitButton').text()).toEqual("Submit");
+    expect(app.find('#submitButton').type()).toEqual("button");
+  });
+
+  it('should have an empty input', () => {
+    expect(app.find('#numberInput').text()).toEqual("");
+    expect(app.find('#numberInput').type()).toEqual("input");
+    expect(app.find('#numberInput').prop('type')).toEqual("number");
+  });
+
+  it('should have an empty label initially', () => {
+    expect(app.find('#result').text()).toEqual("");
+    expect(app.find('#result').type()).toEqual("label");
+  });
+
+  it('should contain the label with instructions', () => {
+    expect(app.find('#instructions').text()).toEqual("Enter a number in the text area to play!")
+    expect(app.find('#instructions').type()).toEqual("label");
+  });
 });
