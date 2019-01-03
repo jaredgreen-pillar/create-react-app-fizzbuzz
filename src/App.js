49d87f9caac1988ff39630e21446680e058ecfb3
jaredgreen <jgreen2@pillartechnology.com>
2019-01-03 16:30:04 -0600

    Implemented the rest of Fizzbuzz and connected it to the UI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      resultText: '',
+      inputValue: ''
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,13 +18,42 @@ class App extends Component {
         <label id='instructions'>Enter a number in the text area to play!</label>
         <br></br>
         <br></br>
-        <input id='numberInput' type="number"></input>
+        <input id='numberInput' type="number" onChange={this.updateInputValue}></input>
         <br></br>
-        <button id='submitButton'>Submit</button>
+        <button id='submitButton' onClick={this.onClickHandler}>Submit</button>
         <br></br>
-        <label id='result'></label>
-      </div>
+        <label id='result'>{this.state.resultText}</label>
+      </div >
     );
+  }
+
+  onClickHandler = () => {
+    this.setState({
+      resultText: this.calculateFizzbuzz(this.state.inputValue)
+    });
+  }
+
+  updateInputValue = (evt) => {
+    this.setState({
+      inputValue: evt.target.value
+    });
+  }
+
+  calculateFizzbuzz(number) {
+    if(number === "") return "";
+
+    switch (0) {
+      case number:
+        return number.toString()
+      case (number % 15):
+        return 'fizzbuzz';
+      case (number % 3):
+        return 'fizz'
+      case (number % 5):
+        return 'buzz'
+      default:
+        return number.toString();
+    }
   }
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -39,3 +39,86 @@ describe('Page Contains Basic Elements', () => {
     expect(app.find('#instructions').type()).toEqual("label");
   });
 });
+
+describe('calculateFizzbuzz', () => {
+  let app;
+
+  beforeEach(() => {
+    app = new App();
+  });
+
+  it('returns the given number', () => {
+    expect(app.calculateFizzbuzz(1)).toEqual("1");
+  });
+
+  it('given a 3 returns a fizz', () => {
+    expect(app.calculateFizzbuzz(3)).toEqual("fizz");
+  });
+
+  it('given a 5 returns a buzz', () => {
+    expect(app.calculateFizzbuzz(5)).toEqual("buzz");
+  });
+
+  it('given a multiple of three, returns fizz', () => {
+    expect(app.calculateFizzbuzz(3)).toEqual("fizz");
+    expect(app.calculateFizzbuzz(33)).toEqual("fizz");
+    expect(app.calculateFizzbuzz(12)).toEqual("fizz");
+    expect(app.calculateFizzbuzz(3333)).toEqual("fizz");
+    expect(app.calculateFizzbuzz(9)).toEqual("fizz");
+  });
+
+  it('given a multiple of five, returns buzz', () => {
+    expect(app.calculateFizzbuzz(5)).toEqual("buzz");
+    expect(app.calculateFizzbuzz(55)).toEqual("buzz");
+    expect(app.calculateFizzbuzz(25)).toEqual("buzz");
+    expect(app.calculateFizzbuzz(5555)).toEqual("buzz");
+    expect(app.calculateFizzbuzz(95)).toEqual("buzz");
+  });
+
+  it('given a 15, returns fizzbuzz', () => {
+    expect(app.calculateFizzbuzz(15)).toEqual("fizzbuzz");
+  });
+
+  it('given a multiple of 15, returns fizzbuzz', () => {
+    const randomInt = Math.ceil((Math.random() + .01) * 100);
+    expect(app.calculateFizzbuzz(15 * randomInt)).toEqual("fizzbuzz");
+  });
+
+  it('given empty string, returns empty string', () => {
+    expect(app.calculateFizzbuzz("")).toEqual("");
+  });
+
+  it('given zero, returns empty string', () => {
+    expect(app.calculateFizzbuzz(0)).toEqual("0");
+  });
+});
+
+describe('Fizzbuzz Integration tests', () => {
+  var app;
+
+  beforeEach(() => {
+    app = mount(<App />)
+  });
+
+  it('given the submit button is clicked, the result label should be updated to display a value', () => {
+    app.find('#numberInput').simulate('change', { target: { value: '1' } });
+    app.find('#submitButton').simulate('click')
+    app.update();
+    expect(app.find('#result').text()).toBeTruthy();
+  });
+
+  it('given an input value when the submit button is clicked, the result label should be updated to display a value', () => {
+    app.find('#numberInput').simulate('change', { target: { value: '9' } });
+    app.find('#submitButton').simulate('click')
+    app.update();
+    expect(app.find('#result').text()).toEqual('fizz');
+  });
+
+  it('given an input value 15 when the submit button is clicked, the result label should be updated to display fizzbuzz', () => {
+    app.find('#numberInput').simulate('change', { target: { value: '15' } });
+    app.find('#submitButton').simulate('click')
+    app.update();
+    expect(app.find('#result').text()).toEqual('fizzbuzz');
+  });
+
+});
